bdeafec96e4ffa90ba1662176433440907a7e699
Jeremiah Wittevrongel <jeremiah@wittevrongel.fake>
2013-09-12 00:14:01 -0700

    Add migration creation logic to MM.create()

--- app/lib/db/MigrationManager.js
+++ app/lib/db/MigrationManager.js
@@ -2,7 +2,19 @@
 
 var fs = require('fs'),
     path = require('path');
-	    
+
+var baseDirectory = path.join(__dirname, '..', '..');
+var relativeMigrationDirectory = path.join('db', 'migrations');
+var absoluteMigrationDirectory = path.join(baseDirectory, relativeMigrationDirectory);
+
+function padNumeral(numeral) {
+    return Array(7 - numeral.toString().length).join('0') + numeral;
+};
+
+function makeNameFilename(migrationName) {
+	return migrationName.replace(/\s+/g, '-');
+};
+
 exports.up = function(migrationName) {
 };
 
@@ -10,22 +22,37 @@ exports.down = function(migrationName) {
 };
 
 exports.create = function(migrationName) {
-	var migrationTemplate = [
-		'"use strict"',
-		'',
-		'exports.up = function(mongoose, next) {',
-		'    next();',
-		'}',
-		'',
-		'exports.down = function(mongoose, next) {',
-		'    next();',
-		'}',
-		''
-	].join('\n');
-	
-	
-	
+    var migrationTemplate = [
+        '"use strict"',
+        '',
+        'exports.up = function(mongoose, next) {',
+        '    next();',
+        '}',
+        '',
+        'exports.down = function(mongoose, next) {',
+        '    next();',
+        '}',
+        ''
+    ].join('\n');
+
+    try {
+        fs.mkdirSync(absoluteMigrationDirectory, parseInt('0775', 8));
+    } catch (err) {
+        // ignore error creating directory
+    }
+
+	var existingMigrationOrdinals = fs.readdirSync(absoluteMigrationDirectory).map(function(filename) {
+        return parseInt(filename.match(/^(\d+)/)[1], 10);
+    }).sort(function(a, b) {
+        return a - b;	
+	});
+    
+	var nextOrdinal = (existingMigrationOrdinals.pop() || 0) + 1;
+    var fileName = padNumeral(nextOrdinal) + "-" + makeNameFilename(migrationName) + ".js";
+	var absoluteFileName = path.join(absoluteMigrationDirectory, fileName);
+	fs.writeFileSync(absoluteFileName, migrationTemplate, {mode: parseInt('0664', 8)});
+	console.log("New migration created: " + path.join(relativeMigrationDirectory, fileName));
 };
 
-	
+    
 
