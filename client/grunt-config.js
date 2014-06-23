7670d7645e164e06d1cd2d197fdb1dd60df4eac0
Jeremiah Wittevrongel <jeremiah@wittevrongel.fake>
2014-06-22 18:08:09 -0700

    jshint for client

--- client/grunt-config.js
+++ client/grunt-config.js
@@ -1,22 +1,34 @@
-"use strict";
+/* global module */
+(function(module) {
+	"use strict";
 
-module.exports = {
-	config: {
-		sass: {
-			client: {
-				options: {
-					precision: 10
-				},
-				files: [{
-        			expand: true,
-        			cwd: 'client/css',
-        			src: ['*.scss'],
-        			dest: 'client/css',
-     			    ext: '.css'
-      			}]
+	module.exports = {
+		config: {
+			sass: {
+				client: {
+					options: {
+						precision: 10
+					},
+					files: [{
+						expand: true,
+						cwd: 'client/css',
+						src: ['*.scss'],
+						dest: 'client/css',
+						ext: '.css'
+					}]
+				}
+			},
+		
+			jshint: {
+				client: {
+					options: {
+						jshintrc: true
+					},
+					src: ['client/**/*.js', '!client/lib/**/*']
+				}
 			}
-		}
-	},
-	npmPlugins: ['grunt-contrib-sass'],
-	tasks: ['sass']
-};
+		},
+		npmPlugins: ['grunt-contrib-sass', 'grunt-contrib-jshint'],
+		tasks: ['jshint', 'sass']
+	};
+})(module);
--- client/js/login.js
+++ client/js/login.js
@@ -1,3 +1,4 @@
-"use strict";
-
-angular.module('login', []);
+(function(angular) {
+	"use strict";
+	angular.module('login', []);
+})(angular);
