e7ad397465087137e427e3b0bb8a98f5e471d014
Jeremiah Wittevrongel <jeremiah@wittevrongel.fake>
2014-07-20 13:52:05 -0700

    update to express 4

--- server/app.js
+++ server/app.js
@@ -6,43 +6,53 @@ var express = require('express'),
     passport = require('passport'),
     authentication = require('./authentication'),
     config = require ('./config'),
-    mongoose = require('mongoose');
-
+    mongoose = require('mongoose'),
+    io = require('socket.io'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	morgan = require('morgan'),
+	compression = require('compression'),
+	errorHandler = require('errorhandler');
+	
 var app = express();
 
-app.configure(function() {
-    app.set('port', config.port || 3000);
-    app.use(express.logger('dev'));
-    app.use(express.cookieParser(config.session.secrets.cookie));
-    app.use(express.bodyParser());
-    app.use(express.session({ 
-        secret: config.session.secrets.express, 
-        cookie: {
-            path: '/',
-            httpOnly: true,
-            maxAge: config.session.timeout
-        }
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(app.router);
-    app.use(authentication);
-    app.use(express.static(path.join(__dirname, 'client')));
+app.set('port', config.port || 3000);
+app.use(morgan('dev'));
+app.use(cookieParser(config.session.secrets.cookie));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ 
+	secret: config.session.secrets.express, 
+	cookie: {
+		path: '/',
+		httpOnly: true,
+		maxAge: config.session.timeout
+	},
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(authentication);
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'client')));
 
-    // add error handler in development only
-    if ('development' == app.get('env')) {
-        app.use(express.errorHandler());
-    }
-});
+// add error handler in development only
+if ('development' == app.get('env')) {
+    app.use(errorHandler());
+}
 
 mongoose.connect(config.db.connectionString);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.Server(app);
+io.listen(server);
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-// for now, put routes here
-// I expect to refactor at some point, but I don't know how it should look yet.
+// routes
 
 // login page
 app.get('/login', function (req, res) {
