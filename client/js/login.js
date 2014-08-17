aeaecbbc04f9e6625c051fac8a3f0744b3b8a378
Jeremiah Wittevrongel <jeremiah@wittevrongel.fake>
2014-08-16 23:09:30 -0700

    get basic validation working in login page

--- client/js/login.js
+++ client/js/login.js
@@ -1,10 +1,28 @@
 (function(angular) {
 	"use strict";
 	angular.module('playchaser.login', [])
-	
-		.controller('loginFormCtrl', function() {
-		})
-		
-		.controller('signupFormCtrl', function() {
+		.directive('pcValidateEqual', function() {
+			return {
+				restrict: 'A',
+				require: 'ngModel',
+				link: function($scope, $element, $attrs, ngModelCtrl) {
+					$scope.$watch($attrs.pcValidateEqual, function(newValue) {
+						if (angular.isDefined(ngModelCtrl.$viewValue) && ngModelCtrl.$viewValue.length) {
+							ngModelCtrl.$setValidity('pcValidateEqual', newValue === ngModelCtrl.$viewValue);
+						}
+					});
+					
+					ngModelCtrl.$parsers.push(function(newValue) {
+						if (!(angular.isDefined(newValue) && newValue.length)) {
+							ngModelCtrl.setValidity('pcValidateEqual', true);
+							return newValue;
+						}
+						var isValid = (newValue === $scope.$eval($attrs.pcValidateEqual));
+						ngModelCtrl.$setValidity('pcValidateEqual', isValid);
+						return isValid ? newValue : undefined;
+					});
+				}
+			};
 		});
+		
 })(angular);
